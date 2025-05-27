e7c822148a466643596b8dda7803644c8d1d8101
avilaha8 <2141@Avi>
2025-05-27 23:02:12 +0530

    Deleted all project files

--- dashBoard/WEB-INF/classes/static/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleBtn = document.getElementById('toggleBtn');
-    const sidebar = document.getElementById('sidebar');
-
-    toggleBtn.addEventListener('click', function() {
-        sidebar.classList.toggle('hidden');
-    });
-});
